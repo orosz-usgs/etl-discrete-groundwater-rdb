1052ae1f895f7fb0917397b9ffb9c34721957602
eorosz <eorosz@contractor.usgs.gov>
2020-12-03 15:17:37 -0500

    IOW-728  Updated SnsUtil to be more robust, just print errors and continue

--- src/main/java/gov/usgs/wma/waterdata/groundwater/SnsUtil.java
+++ src/main/java/gov/usgs/wma/waterdata/groundwater/SnsUtil.java
@@ -2,6 +2,7 @@ package gov.usgs.wma.waterdata.groundwater;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
@@ -19,50 +20,65 @@ public class SnsUtil {
 	private final AmazonSNS snsClient = AmazonSNSClientBuilder.defaultClient();
 	private final Topic snsTopic = getSNSTopic();
 
-	@Autowired
 	private Properties properties;
 
+	@Autowired
 	SnsUtil(Properties properties) {
 		this.properties = properties;
 	}
 
 	/**
 	 * Simple helper method to send a message to the etl discrete groundwater rdb
-	 * SNS topic.
+	 * SNS topic. Exceptions are caught and logged to standard error, so that the rdb
+	 * processing continues.
 	 *
 	 * @param mess The message to place in the SNS topic.
 	 */
 	public void publishSNSMessage(String mess) {
-		PublishRequest request = new PublishRequest(mess, snsTopic.getTopicArn());
-
-		try {
-			snsClient.publish(request);
-		} catch (Exception e) {
-			throw new RuntimeException("Error publishing SNS topic message: " + e.getMessage(), e);
+		if (snsTopic != null) {
+			try {
+				PublishRequest request = new PublishRequest(mess, snsTopic.getTopicArn());
+				snsClient.publish(request);
+			} catch (Exception e) {
+				System.err.print("Error publishing message to SNS topic: " + e.getMessage());
+				System.err.print("Message to have been sent: " + mess);
+				e.printStackTrace();
+			}
+		} else {
+			System.err.print("Error SNS logging not initialized, message to have been sent: " + mess);
 		}
 	}
 
 	private Topic getSNSTopic() {
-		String topicName = String.format("%s-%s-topic", TOPIC_BASE_NAME, properties.getTier());
 		Topic snsTopic = null;
+		String tier = properties == null ? null : properties.getTier();
+		String mess = "";
 
-		try {
-			ListTopicsRequest request = new ListTopicsRequest();
+		if (StringUtils.hasText(tier)) {
+			try {
+				String topicName = String.format("%s-%s-topic", TOPIC_BASE_NAME, tier);
+				ListTopicsRequest request = new ListTopicsRequest();
 
-			ListTopicsResult result = snsClient.listTopics(request);
-			for (Topic topic : result.getTopics()) {
-				String arn = topic.getTopicArn();
-				if (arn != null && arn.contains(topicName)) {
-					snsTopic = topic;
-					break;
+				ListTopicsResult result = snsClient.listTopics(request);
+				for (Topic topic : result.getTopics()) {
+					String arn = topic.getTopicArn();
+					if (arn != null && arn.contains(topicName)) {
+						snsTopic = topic;
+						break;
+					}
 				}
-			}
 
-			if (snsTopic == null) {
-				throw new RuntimeException("SNS topic not found: " + topicName);
+				if (snsTopic == null) {
+					System.err.println("Error initializing SNS logging: SNS topic not found: " + topicName);
+				}
+			} catch (Exception e) {
+				System.err.println("Error getting SNS topic: " + e.getMessage());
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			throw new RuntimeException("Error getting SNS topic: " + e.getMessage(), e);
+		} else {
+			// Todo: use logging framework
+			mess = properties == null ? "properties component not available" : "tier property not set";
+			System.err.print("Error initializing SNS logging: " + mess);
 		}
 
 		return snsTopic;
